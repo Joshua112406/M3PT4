b9987365fb287132129a76c45daa896018a24e2a
Joshua112406 <liawjoshua33@gmail.com>
2025-03-10 20:01:34 +0800

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,7 +1,7 @@
 public class Task2 {
     public static void main(String[] args) {
         int[] numbers = {10, 20, 30, 40, 50};
-        numbers[1] = 25; // Modify the second element (index 1)
+        numbers[1] = 25;
 
         System.out.print("Updated array: ");
         for (int num : numbers) {
